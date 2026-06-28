0ee07ce6a4f10b5f382f2a45b5ebf5df1a1e302b
NicoT111 <nico@tscheschlock.de>
2026-06-28 17:04:43 +0200

    Initial Commit

---
+++ SnapText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapText
+//
+//  Created by Nico Tscheschlock on 28.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapText/SnapTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapTextApp.swift
+//  SnapText
+//
+//  Created by Nico Tscheschlock on 28.06.26.
+//
+
+import SwiftUI
+
+@main
+struct SnapTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
